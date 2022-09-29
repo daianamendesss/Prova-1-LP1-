e74a4fe54a74fa3ae1a9a412ee6219987b4d27a3
Daiana Mendes <103112318+daianamendesss@users.noreply.github.com>
2022-09-29 09:01:34 -0300

    Add files via upload

---
+++ ex.01 prova.1.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+void printVet(int v[], int n){
+	int i;
+	for(i=0;i<n;i++)
+	   printf("%2d | ",v[i]);
+	   int n1,n2,n3,n4,n5,cont=0,media;
+	printf("\nDigite a nota 1:\n");
+	scanf("%d",&n1);
+	printf("\nDigite a nota 2:\n");
+	scanf("%d",&n2);
+	printf("\nDigite a nota 3:\n");
+	scanf("%d",&n3);
+	printf("\nDigite a nota 4:\n");
+	scanf("%d", &n4);
+	printf("\nDigite a nota 5:\n");
+	scanf("%d",&n5);
+	printf("\n As notas obtidas pelo aluno foram:%d ", &n1);
+	printf("\n%d ", &n2);
+	printf("\n%d ", &n3);
+	printf("\n%d ", &n4);
+	printf("\n%d ", &n5);
+		media=(n1+n2+n3+n4+n5)/5;
+	
+	printf("\n As nota final obtida pelo aluno foi: ", media); 
+	
+}
+	return 0;
+
+
